44333436fc46b1212aa3d5309dd908fc9449b3f2
yasmin-coder-1510 <yasminakter20031@gmail.com>
2026-03-29 09:40:25 +0600

    Add files via upload

---
+++ Backslash chacter.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+ cout<<"Yasmin Akter"<<endl <<"Noakhali,Bangladesh" <<endl <<"Noakhali Science and Technology University" ;
+ cout<<"\n";
+ cout<<"\tSchool :\'Thanar Hat High School\'\n";
+ cout<<"College :\'Noakhali Goverment College\'\n";
+ cout<<"Mother's name : \"Fatema Begum\"\n";
+ cout<<"Father's name : \"Amir Hossain\"\n";
+
+ getch() ;
+}
